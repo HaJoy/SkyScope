5bbdc6369498d81362713afa3816d501f55272cd
HaJoy <hammercanog@gmail.com>
2025-07-17 19:22:23 -0500

    Theme provider configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 function App() {
   return (
-    <Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
-
+          <Route path="/" element={<Home />} />
         </Routes>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
