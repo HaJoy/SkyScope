ca4fa9823d3078fd4b67b43c95de566bfda01457
HaJoy <hammercanog@gmail.com>
2025-07-17 20:35:48 -0500

    Primera parte de la estructura de la pagina hecha

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { PageDisplay } from '@/components/layout/PageDisplay'
+import { Sun } from 'lucide-react'
 import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <PageDisplay>
+      <div className='flex justify-between bg-card border rounded w-full h-52 p-5'>
+        <div className='flex flex-col justify-center gap-2'>
+          <span className='text-2xl'>Barranquilla</span>
+          <span className='text-7xl'>38</span>
+        </div>
+        <div className='flex flex-col justify-center gap-2'>
+          <Sun size={90} />
+          <span className='text-2xl'>Sunny</span>
+        </div>
+      </div>
+      
+      <div className='flex flex-col sm:flex-row border rounded h-64 w-full gap-3'>
+        <div className='flex sm:flex-1 bg-card border rounded p-2 h-full min-w-52'>
+
+        </div>
+        <div className='flex sm:flex-2 bg-card border rounded p-2 h-full min-w-52'>
+          
+        </div>
+        <div className='flex sm:flex-2 bg-card border rounded p-2 h-full min-w-52'>
+          
+        </div>
+      </div>
+    </PageDisplay>
   )
 }
